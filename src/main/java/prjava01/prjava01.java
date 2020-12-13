b23734807802ebb7de66ba2764a1ea3838af03ab
Weixin Xu <wexu2021daw@protonmaill.com>
2020-12-14 00:41:42 +0100

    Versió 0.2 del projecte prjava01

--- src/main/java/prjava01/prjava01.java
+++ src/main/java/prjava01/prjava01.java
@@ -28,6 +28,7 @@ public class prjava01 {
             bw.write("    </title>");bw.newLine();
             bw.write("  </head>");bw.newLine();
             bw.write("  <body>");bw.newLine();
+            bw.write(" <h1>DAW2 m08uf4eac1</h1>");bw.newLine(); 
             bw.write("    Nova p&agrave;gina web");bw.newLine();
             bw.write("  </body>");bw.newLine();
             bw.write("</html>");bw.newLine();
